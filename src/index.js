bfdacc6f2cdb3b6ab26d4bc883e1d68bc755726d
jnhro1 <jnhro1@gmail.com>
2022-01-10 14:51:49 +0900

    Init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom'; // * BrowserRouter 불러오기
+// import { createStore } from 'redux';
+// import rootReducer from './modules';
 import App from './App';
+// import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
 
+// const store = createStore(rootReducer); // 스토어를 만듭니다.
+// console.log(store.getState()); // 스토어의 상태를 확인해봅시다.
+
 ReactDOM.render(
-  <React.StrictMode>
+  // <Provider store={store}>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
+  // </Provider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
---
+++ src/modules/index.js
@@ -0,0 +1,1 @@
+// import { combindReducers } from 'redux';
