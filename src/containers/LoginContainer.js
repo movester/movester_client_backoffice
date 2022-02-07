dcad820103f76f80e4c14638d95d72583a6befea
ysh2987 <ysh2987@naver.com>
2022-02-08 01:18:42 +0900

    #38 Fix: 디렉토리 구조 변경

--- src/containers/LoginContainer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import LoginComponent from '../components/login/LoginComponent';
-
-function LoginContainer() {
-  return <LoginComponent />;
-}
-
-export default LoginContainer;
