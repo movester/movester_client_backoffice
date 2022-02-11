468d3c299c6d5b10923248d12acc0635a20b1655
ysh2987 <ysh2987@naver.com>
2022-02-11 20:17:03 +0900

    Fix: props change

--- src/dataList/listTableHeaders.js
+++ src/dataList/listTableHeaders.js
@@ -1,7 +1,18 @@
-const UserListHeaders = ['#', '이메일', '이름', '나이', '성별', '직업'];
+const UserListHeaders = ['#', '이름', '이메일', '카카오 아이디', '이메일 인증 여부', '가입 날짜'];
 const EventListHeaders = ['#', '제목', '기간', '당첨자'];
 const UserDetailRecord = ['월', '포인트'];
 const UserDetailAttendance = ['월', '어깨', '다리'];
 const stretchingListHeaders = ['#', '제목', '부위', '자세', '효과', '난이도'];
 
-export { UserListHeaders, EventListHeaders, UserDetailRecord, UserDetailAttendance, stretchingListHeaders };
+const listHeaders = {
+  userHeader: ['#', '이름', '이메일', '카카오 아이디', '이메일 인증 여부', '가입 날짜'],
+};
+
+export {
+  UserListHeaders,
+  EventListHeaders,
+  UserDetailRecord,
+  UserDetailAttendance,
+  stretchingListHeaders,
+  listHeaders,
+};
